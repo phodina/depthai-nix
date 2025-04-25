9c32d898e340c8f026b3b5fec003d38a11af8ef7
Petr Hodina <petr.hodina@luxonis.com>
2025-04-25 11:08:28 +0200

    flake: nixfmt on flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs repository with packaged DepthAIv3 and it's dependencies
-     nixpkgs.url = "github:phodina/nixpkgs?ref=depthaiv3_alpha15";
+    nixpkgs.url = "github:phodina/nixpkgs?ref=depthaiv3_alpha15";
   };
 
   # Give hint for binary cache to download prebuilt packages
@@ -16,35 +16,42 @@
     ];
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
-        # Define Python packages to include in the environment
-        pythonPackages = ps: with ps; [
-          # Basic tools
-          pip
-          setuptools
-          wheel
-          ipython
-          
-          # Development tools
-          pytest
-          black
-          flake8
-          mypy
-          
-          # Add any other packages you need
-          scipy
-          matplotlib
 
-          #NOTE: OpenCV is already added as it has dependency on GTK to show windows
-        ];
-        
+        # Define Python packages to include in the environment
+        pythonPackages =
+          ps: with ps; [
+            # Basic tools
+            pip
+            setuptools
+            wheel
+            ipython
+
+            # Development tools
+            pytest
+            black
+            flake8
+            mypy
+
+            # Add any other packages you need
+            scipy
+            matplotlib
+
+            #NOTE: OpenCV is already added as it has dependency on GTK to show windows
+          ];
+
         # Create a Python with selected packages
         pythonEnv = pkgs.python3.withPackages pythonPackages;
-        
+
         # Ensure the docker image will display GUI Windows
         guiPackages = with pkgs; [
           xorg.libX11
@@ -71,13 +78,14 @@
           gdk-pixbuf
           cairo
         ];
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pythonEnv
             pkgs.depthai-core
           ];
- 
+
           shellHook = ''
             # Get the current working directory
             CURRENT_DIR=$(pwd)
@@ -87,22 +95,22 @@
               echo "Creating virtual environment..."
               ${pkgs.python3}/bin/python -m venv .venv
             fi
-            
+
             # Add the virtual environment to PYTHONPATH
             export PYTHONPATH=$CURRENT_DIR/.venv:$PYTHONPATH
-            
+
             # Add local bin directory to PATH
             export PATH=$CURRENT_DIR/.venv/bin:$PATH
-            
+
             # Display Python information
             echo -e "\e[31mPython development environment activated\e[0m"
             echo -e "\e[31mPython version: $(python --version)\e[0m"
             echo -e "\e[31mDepthAI version: $(python -c "import depthai as dai; print(dai.__version__);")\e[0m"
             echo -e "\e[31mPython path: $(which python)\e[0m"
-            
+
             # You can add project-specific configurations here
             # export PROJECT_HOME=$CURRENT_DIR
-            
+
             # Set custom prompt to indicate we're in a dev shell
             export PS1="(py-dev) \[\033[1;32m\]\w\[\033[0m\] > "
           '';
@@ -127,53 +135,54 @@
 
             guiPackages
           ];
-
           config = {
             Env = [
-              "LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-                pkgs.gcc-unwrapped.lib
-                pkgs.zlib
-                pkgs.wayland-protocols
-                pkgs.xorg.libX11
-                pkgs.xorg.libXcursor
-                pkgs.xorg.libXrandr
-                pkgs.xorg.libXi
-                pkgs.vulkan-loader
-                pkgs.libGL
-                pkgs.glib.out
-              ]}"
+              "LD_LIBRARY_PATH=${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.gcc-unwrapped.lib
+                  pkgs.zlib
+                  pkgs.wayland-protocols
+                  pkgs.xorg.libX11
+                  pkgs.xorg.libXcursor
+                  pkgs.xorg.libXrandr
+                  pkgs.xorg.libXi
+                  pkgs.vulkan-loader
+                  pkgs.libGL
+                  pkgs.glib.out
+                ]
+              }"
               "DISPLAY=:0"
               "QT_X11_NO_MITSHM=1"
               "NO_AT_BRIDGE=1"
               "PYTHONPATH=${pythonEnv}/${pythonEnv.sitePackages}:${pkgs.depthai-core}/${pythonEnv.sitePackages}"
             ];
             WorkingDir = "/app";
-            Cmd = ["${pkgs.bashInteractive}/bin/bash"];
+            Cmd = [ "${pkgs.bashInteractive}/bin/bash" ];
 
             NetworkDisabled = false;
 
             Volumes = {
-              "/dev/bus/usb" = {};
-              "/tmp/.X11-unix" = {};
+              "/dev/bus/usb" = { };
+              "/tmp/.X11-unix" = { };
             };
 
-            CapAdd = ["SYS_PTRACE"];
+            CapAdd = [ "SYS_PTRACE" ];
 
-           extraCommands = ''
-            mkdir -p mnt
-            chmod 777 mnt
-            # Create the entrypoint script to handle user/group setup
-            mkdir -p usr/bin
-            cat > usr/bin/entrypoint.sh << 'EOF'
-            #!/bin/bash
-            set -e
-              USER_ID=${LOCAL_USER_ID:-1000}
-              GROUP_ID=${LOCAL_GROUP_ID:-1000}
-EOF
-            chmod +x usr/bin/entrypoint.sh
-'';
-     };
-};
+            #           extraCommands = ''
+            #            mkdir -p mnt
+            #            chmod 777 mnt
+            #            # Create the entrypoint script to handle user/group setup
+            #            mkdir -p usr/bin
+            #            cat > usr/bin/entrypoint.sh << 'EOF'
+            #            #!/bin/bash
+            #            set -e
+            #              USER_ID=${LOCAL_USER_ID:-1000}
+            #              GROUP_ID=${LOCAL_GROUP_ID:-1000}
+            #EOF
+            #            chmod +x usr/bin/entrypoint.sh
+            #'';
+          };
+        };
       }
     );
 }
